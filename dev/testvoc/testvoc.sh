2fc65e10b85dfa7d21cc4d3636760794309e6852
Marc Riera Irigoyen <marc.riera.irigoyen@gmail.com>
2018-10-03 00:44:55 +0200

    Testvoc: remove temporary files after finishing

--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "==Romanian->Catalan==========================";
 
-bash inconsistency.sh ron-cat ../../../apertium-ron/apertium-ron.ron.dix > /tmp/ron-cat.testvoc; bash inconsistency-summary.sh /tmp/ron-cat.testvoc ron-cat; grep ' #' /tmp/ron-cat.testvoc > testvoc-errors.ron-cat.txt; grep '@' /tmp/ron-cat.testvoc >> testvoc-errors.ron-cat.txt;
+bash inconsistency.sh ron-cat ../../../apertium-ron/apertium-ron.ron.dix > /tmp/ron-cat.testvoc; bash inconsistency-summary.sh /tmp/ron-cat.testvoc ron-cat; grep ' #' /tmp/ron-cat.testvoc > testvoc-errors.ron-cat.txt; grep '@' /tmp/ron-cat.testvoc >> testvoc-errors.ron-cat.txt; rm /tmp/ron-cat.testvoc
 
 if [[ $1 = "-u" ]];
 then
   echo "Looking for bidix entries missing from Romanian monodix …";
-  pushd ../../ > /dev/null; bash dev/testvoc/bidix-unknowns.sh ron | grep -v ":<:" | grep -v "REGEX" > dev/testvoc/testvoc-missing.ron.txt; popd > /dev/null;
+  pushd ../../ > /dev/null; bash dev/testvoc/bidix-unknowns.sh ron | grep -v ":<:" | grep -v "REGEX" | grep -v "<prn><enc>" > dev/testvoc/testvoc-missing.ron.txt; popd > /dev/null;
   echo "Entries missing from monodix: "$(wc -l < testvoc-missing.ron.txt) | tee -a testvoc-summary.ron-cat.txt;
 fi
 echo "";
@@ -14,11 +14,11 @@ echo "";
 
 echo "==Catalan->Romanian==========================";
 
-bash inconsistency.sh cat-ron ../../../apertium-cat/apertium-cat.cat.dix > /tmp/cat-ron.testvoc; bash inconsistency-summary.sh /tmp/cat-ron.testvoc cat-ron; grep ' #' /tmp/cat-ron.testvoc > testvoc-errors.cat-ron.txt; grep '@' /tmp/cat-ron.testvoc >> testvoc-errors.cat-ron.txt;
+bash inconsistency.sh cat-ron ../../../apertium-cat/apertium-cat.cat.dix > /tmp/cat-ron.testvoc; bash inconsistency-summary.sh /tmp/cat-ron.testvoc cat-ron; grep ' #' /tmp/cat-ron.testvoc > testvoc-errors.cat-ron.txt; grep '@' /tmp/cat-ron.testvoc >> testvoc-errors.cat-ron.txt; rm /tmp/cat-ron.testvoc
 
 if [[ $1 = "-u" ]];
 then
   echo "Looking for bidix entries missing from Catalan monodix …";
-  pushd ../../ > /dev/null; bash dev/testvoc/bidix-unknowns.sh cat | grep -v ":<:" | grep -v "REGEX" > dev/testvoc/testvoc-missing.cat.txt; popd > /dev/null;
+  pushd ../../ > /dev/null; bash dev/testvoc/bidix-unknowns.sh cat | grep -v ":<:" | grep -v "REGEX" | grep -v "<prn><enc>" > dev/testvoc/testvoc-missing.cat.txt; popd > /dev/null;
   echo "Entries missing from monodix: "$(wc -l < testvoc-missing.cat.txt) | tee -a testvoc-summary.cat-ron.txt;
 fi

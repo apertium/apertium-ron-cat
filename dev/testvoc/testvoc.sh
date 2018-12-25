d5eaf1e73f33541d618c56652785827c817a804b
Marc Riera Irigoyen <marc.riera.irigoyen@gmail.com>
2018-12-25 22:58:22 +0100

    Upgrade testvoc scripts

--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
-echo "==Romanian->Catalan==========================";
 
-bash inconsistency.sh ron-cat ../../../apertium-ron/apertium-ron.ron.dix > /tmp/ron-cat.testvoc; bash inconsistency-summary.sh /tmp/ron-cat.testvoc ron-cat; grep ' #' /tmp/ron-cat.testvoc > testvoc-errors.ron-cat.txt; grep '@' /tmp/ron-cat.testvoc >> testvoc-errors.ron-cat.txt; rm /tmp/ron-cat.testvoc
-
-if [[ $1 = "-u" ]];
-then
-  echo "Looking for bidix entries missing from Romanian monodix …";
-  pushd ../../ > /dev/null; bash dev/testvoc/bidix-unknowns.sh ron | grep -v ":<:" | grep -v "REGEX" | grep -v "<prn><enc>" > dev/testvoc/testvoc-missing.ron.txt; popd > /dev/null;
-  echo "Entries missing from monodix: "$(wc -l < testvoc-missing.ron.txt) | tee -a testvoc-summary.ron-cat.txt;
+if ! [[ -e testvoc.conf ]]; then
+    echo "Testvoc configuration file (testvoc.conf) not found."
+    exit 1
 fi
-echo "";
 
+while getopts "eq" opt; do
+  case $opt in
+    e)
+      ENCLITICS=true  # If the -e flag is used, enclitics are skipped for faster processing
+      ;;
+    q)
+      QUIET=true  # If the -q flag is used, no summary is generated
+      ;;
+  esac
+done
 
-echo "==Catalan->Romanian==========================";
+IFS=","
+modes=($(grep -m 1 "^PairModes=" testvoc.conf | cut -d = -f 2))
+names=($(grep -m 1 "^PairNames=" testvoc.conf | cut -d = -f 2))
+unset IFS
 
-bash inconsistency.sh cat-ron ../../../apertium-cat/apertium-cat.cat.dix > /tmp/cat-ron.testvoc; bash inconsistency-summary.sh /tmp/cat-ron.testvoc cat-ron; grep ' #' /tmp/cat-ron.testvoc > testvoc-errors.cat-ron.txt; grep '@' /tmp/cat-ron.testvoc >> testvoc-errors.cat-ron.txt; rm /tmp/cat-ron.testvoc
+for i in "${!modes[@]}";
+do
+    echo ""
+    printf "== %.45s\n" "${names[$i]} ============================================"
+    if [[ $ENCLITICS ]]; then
+        bash inconsistency.sh -e ${modes[$i]} auto > .testvoc
+    else
+        bash inconsistency.sh ${modes[$i]} auto > .testvoc
+    fi
+    grep ' #' .testvoc > testvoc-errors.${modes[$i]}.txt
+    if ! [[ $QUIET ]]; then
+        bash inconsistency-summary.sh .testvoc ${modes[$i]}
+    fi
+    rm .testvoc
+done
 
-if [[ $1 = "-u" ]];
-then
-  echo "Looking for bidix entries missing from Catalan monodix …";
-  pushd ../../ > /dev/null; bash dev/testvoc/bidix-unknowns.sh cat | grep -v ":<:" | grep -v "REGEX" | grep -v "<prn><enc>" > dev/testvoc/testvoc-missing.cat.txt; popd > /dev/null;
-  echo "Entries missing from monodix: "$(wc -l < testvoc-missing.cat.txt) | tee -a testvoc-summary.cat-ron.txt;
-fi
+exit 0
